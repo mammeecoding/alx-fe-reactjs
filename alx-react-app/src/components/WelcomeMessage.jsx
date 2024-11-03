91adbb2093caef7741bee0fa308e4b5457c4da3a
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-11-03 16:53:59 +0300

    Update WelcomeMessage.jsx

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,10 +1,21 @@
 function WelcomeMessage() {
   return (
+      <>
+      <header>
+        <h1>My Favorite Cities</h1>
+      </header>
+      <main>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
       <div>
-          <h1>Hello everyone, I am learning React at ALX!</h1>
+          <h1>Hello Everyone</h1>
           <p>I am learning about JSX!</p>
           <p>This is a simple JSX component.</p>
       </div>
+      <footer>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    </>
   );
 }
 
