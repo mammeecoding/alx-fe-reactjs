4d32cb927d9f5986f5bc460466fb4494da37d3ed
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-11-03 17:14:01 +0300

    Add files via upload

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+function WelcomeMessage() {
+  return (
+      <div>
+          <h1>Hello Everyone</h1>
+          <p>I am learning about JSX!</p>
+          <p>This is a simple JSX component.</p>
+      </div>
+      
+  
+  );
+}
+
+export default WelcomeMessage;
