7feb6001e38e9153c3000afbbc84475f8375b662
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-11-10 13:13:33 +0300

    Update UserDetails.jsx

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,9 +1,13 @@
-function UserDetails({ userData }) {
+import UserContext from "../UserContext";
+
+function UserDetails({ useContext }) {
   return (
     <div>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
+    useContext(UserContext)
+    UserContext()
   );
 }
 
