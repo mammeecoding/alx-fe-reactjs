d611a0777bc477e3217089ac4a831e62854ffb77
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-11-10 15:05:34 +0300

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+element.path
+
 Routes
 Route
 BrowserRouter
