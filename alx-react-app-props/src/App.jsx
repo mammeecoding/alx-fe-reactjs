5d5c4a710fc3083ee497e1cbeeb157487d975adc
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-11-10 13:05:02 +0300

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,7 +3,7 @@ import UserContext from './UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
+  UserContext.Provider(userData ,"value")
   return <ProfilePage userData={userData} />;
       <UserContext/>
 }
