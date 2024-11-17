95c1154d84e67b17556620b6737789ec6f039fad
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-11-17 17:37:00 +0300

    Update RecommendationsList.jsx

--- recipe-sharing-app/src/components/RecommendationsList.jsx
+++ recipe-sharing-app/src/components/RecommendationsList.jsx
@@ -1,1 +1,23 @@
+import { useRecipeStore } from './recipeStore';
 
+const RecommendationsList = () => {
+  const recomendation = useRecipeStore(state => state.recomendation.map(id =>
+    state.recipes.find(recipe => recipe.id === id)
+  ));
+
+  return (
+    <div>
+      <h2>Recomendation</h2>
+      {recomendation.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default RecommendationsList
+
+
+useRecipeStore", "map"
