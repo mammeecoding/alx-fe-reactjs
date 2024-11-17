2633f90f61cb82df1fd8fbb48ded961ee7b1c6a1
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-11-17 17:49:41 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,8 +5,22 @@ const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
+
+    const SearchBar = () => {
+  const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
+
+  return (
+    <input
+      type="text"
+      placeholder="Search recipes..."
+      onChange={(e) => setSearchTerm(e.target.value)}
+    />
+  );
+};
+    
     return(
   <div>
+    
     <updateRecipe/>
     </deleteRecipe>
   </div>
