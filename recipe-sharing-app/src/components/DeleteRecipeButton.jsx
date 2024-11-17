70ba0ddb56cc7a2a23b96bc91146ecb9e3fe1c11
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-11-17 15:24:58 +0300

    Update DeleteRecipeButton.jsx

---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,8 @@
+import { useRecipeStore,useNavigate,deleteRecipe } from './recipeStore';
+const DeleteRecipeButton = ({ recipId}) => {
+
+}
+return(
+  <div>
+  <button>delete a recipe</button>
+  </div>)
