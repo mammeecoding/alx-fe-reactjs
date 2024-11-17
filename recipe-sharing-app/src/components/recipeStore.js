8a8cb8ce0a2a86d5538e417e6e4337f6e470b418
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-11-17 17:41:56 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,14 +1,9 @@
 import create from 'zustand'
+import DeleteRecipe from /DeleteRecipe.jsx
 const useRecipeStore = create(set => ({
   recipes: [],
-  searchTerm: '',
-  setSearchTerm: (term) => set({ searchTerm: term }),
-  filteredRecipes: [],
-  filterRecipes: () => set(state => ({
-    filteredRecipes: state.recipes.filter(recipe =>
-      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
-    ))
-  })),
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+  
 }));
 export default useRecipeStore;
-
